e40ebf4c92a26d02eb754cb6b60c4b9e4b9e504f
Emil Julian Costinea <ejc@arp-hansen.dk>
2023-04-11 15:50:50 +0200

    added user register and login

---
+++ Dtos/User/UserLoginDto.cs
@@ -0,0 +1,7 @@
+﻿namespace rpg_backend.Dtos.User;
+
+public class UserLoginDto
+{
+    public string Username { get; set; } = string.Empty;
+    public string Password { get; set; } = string.Empty;
+}
--- Models/User.cs
+++ Models/User.cs
@@ -3,7 +3,8 @@
 public class User
 {
     public int Id { get; set; }
-    public string Username { get; set; }
+    public string Username { get; set; } = string.Empty;
     public byte[] PasswordHash { get; set; }
     public byte[] PasswordSalt { get; set; }
+    public List<Character>? Characters { get; set; }
 }
