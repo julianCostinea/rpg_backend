d430cb42b1fcdcd8c4bda864b84e18dc0bc2ca98
Emil Julian Costinea <ejc@arp-hansen.dk>
2023-04-18 20:24:49 +0200

    added Weapon model

---
+++ Models/Weapon.cs
@@ -0,0 +1,9 @@
+﻿namespace rpg_backend.Models;
+
+public class Weapon
+{
+    public int Id { get; set; }
+    public string Name { get; set; } = "Fists";
+    public int Damage { get; set; } = 10;
+    public Character Character { get; set; }
+}
